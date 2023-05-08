3b54d40cd5d5274ea767062993f6a539f5fe610c
ThierryGuetta <andrey.stolyarov1337@gmail.com>
2023-05-08 22:27:56 +0300

    ...

--- src/main/java/com/example/springsecurityapplication/config/UploadsConfig.java
+++ src/main/java/com/example/springsecurityapplication/config/UploadsConfig.java
@@ -8,8 +8,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class UploadsConfig implements WebMvcConfigurer {
-    @Value("${uploads.path}")
-    private String uploadsPath;
+//    @Value("${uploads.path}")
+//    private String uploadsPath;
+    private String uploadsPath = "C:\\Users\\Андрей\\Desktop\\Spring\\DietShopProject\\uploads\\";
 
     @Override
     public void addResourceHandlers(@NotNull ResourceHandlerRegistry registry) {
